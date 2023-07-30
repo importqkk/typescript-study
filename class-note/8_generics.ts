d80bec6841193144bd30594ddac17f176d0c18f4
importqkk <import_qkk@naver.com>
2023-07-30 17:15:57 +0900

    제네릭

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -15,3 +15,28 @@ function logText<T>(text: T): T {
     return text;
 }
 logText<string>('10');
+
+// 인터페이스에 제네릭을 선언하는 방법
+interface Dropdown<T> {
+    value: T;
+    selected: boolean;
+}
+const obj: Dropdown<string> = { value: 'abc', selected: false }
+
+// 제네릭의 타입 제한
+function logTextLength<T>(text: T[]): T[] {
+    console.log(text.length);
+    return text;
+}
+logTextLength<string>(['hi', 'abc']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+function logTextLen<T extends LengthType>(text: T): T {
+    text.length;
+    return text;
+}
+logTextLen('a');
+logTextLen({ length: 10 })
